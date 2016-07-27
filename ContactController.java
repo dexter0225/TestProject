3b2ee17b0682c6109f1c67a5680a1b70cba583ac
dexter0225 <dexter_flores@yahoo.com>
2016-07-27 15:03:39 +0800

    Add test java file

---
+++ ContactController.java
@@ -0,0 +1,22 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ContactController {
+
+	@RequestMapping(value = "/searchContact")
+	public ModelAndView searchContact() {
+		System.out.println("Inside searchContact()");
+		//ModelAndView mav = new ModelAndView("showContact");
+		String message = "<br><div style='text-align:center;'>"
+		+ "<h3>********** Hello World, Spring MVC Tutorial</h3>"
+		+ "This message is coming from CrunchifyHelloWorld.java **********"
+		+ "</div><br><br>";		
+		return new ModelAndView("replyContact", "message", message);	
+	}
+
+}
